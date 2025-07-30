bf0b9f868868bebde912274637b312471d97594a
Gabor Markowski <gabor94@gmail.com>
2025-07-30 14:15:22 +0100

    2411: a little faster

--- 2411/src/lib.rs
+++ 2411/src/lib.rs
@@ -1,19 +1,22 @@
 struct Solution {}
 
 impl Solution {
-    pub fn add(acc: &mut Vec<i32>, mut el: i32) {
+    #[inline]
+    pub fn add(acc: &mut Vec<i16>, mut el: i32) {
         for idx in 0..acc.len() {
-            acc[idx] += el % 2;
+            acc[idx] += (el % 2) as i16;
             el /= 2;
         }
     }
-    pub fn remove(acc: &mut Vec<i32>, mut el: i32) {
+    #[inline]
+    pub fn remove(acc: &mut Vec<i16>, mut el: i32) {
         for idx in 0..acc.len() {
-            acc[idx] -= el % 2;
+            acc[idx] -= (el % 2) as i16;
             el /= 2;
         }
     }
-    pub fn check(acc: &Vec<i32>, mut el: i32) -> bool {
+    #[inline]
+    pub fn check(acc: &Vec<i16>, mut el: i32) -> bool {
         for idx in 0..acc.len() {
             if el % 2 == 1 {
                 if acc[idx] == 0 {
@@ -26,38 +29,24 @@ impl Solution {
     }
     pub fn smallest_subarrays(nums: Vec<i32>) -> Vec<i32> {
         let mut res = vec![nums.len() as i32 + 2; nums.len()];
-        let (mut start_iter, mut end_iter) = (nums.iter().rev(), nums.iter().rev().peekable());
-        let mut acc = vec![0; 22];
+        let start_iter = nums.iter().enumerate().rev();
+        let mut end_iter = nums.iter().rev().peekable();
+        let mut acc = vec![0 as i16; 30];
         let mut size = 0;
         let mut or_val = 0;
-        let mut idx = res.len() as i32 - 1;
 
-        let mut acc_vals: Vec<i32> = vec![];
-        loop {
-            let el_option = start_iter.next();
-            if el_option.is_none() {
-                break;
-            }
-            let el = el_option.unwrap();
+        for (idx, el) in start_iter {
             or_val |= *el;
             Self::add(&mut acc, *el);
             size += 1;
-            res[idx as usize] = res[idx as usize].min(size);
 
             // try removing end element
-            while size > 1 {
-                let end_el = *end_iter.peek().unwrap();
+            while Self::check(&acc, or_val) && size > 0 {
+                let end_el = end_iter.next().unwrap();
                 Self::remove(&mut acc, *end_el);
-                if !Self::check(&acc, or_val) {
-                    Self::add(&mut acc, *end_el);
-                    break;
-                } else {
-                    size -= 1;
-                    end_iter.next();
-                }
+                size -= 1;
             }
-            res[idx as usize] = res[idx as usize].min(size);
-            idx -= 1;
+            res[idx as usize] = res[idx as usize].min(size + 1);
         }
         return res;
     }
@@ -78,6 +67,13 @@ mod tests {
     fn test2() {
         let nums = vec![1, 2];
         let res = vec![2, 1];
+        assert_eq!(Solution::smallest_subarrays(nums), res);
+    }
+
+    #[test]
+    fn test3() {
+        let nums = vec![0];
+        let res = vec![1];
         assert_eq!(Solution::smallest_subarrays(nums), res);
     }
 }
