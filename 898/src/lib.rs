2cb49d0dc46cea89b5361f7df7b9a89a3562366a
Gabor Markowski <gabor94@gmail.com>
2025-07-31 12:59:11 +0100

    898: a little faster, using HashSet rather than HashMap

--- 898/src/lib.rs
+++ 898/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 struct Solution {}
 
@@ -8,8 +8,6 @@ impl Solution {
         let mut bit_location = vec![arr.len() + 100; 33];
 
         for (idx, num) in arr.iter().enumerate() {
-            // println!("num: {}", num);
-
             // update the bit locations
             let mut bit_idx = 1;
             if *num == 0 {
@@ -24,37 +22,23 @@ impl Solution {
                     bit_idx += 1;
                 }
             }
-            // println!("bit location: {:?}", bit_location);
 
-            let mut x: HashMap<usize, i32> = HashMap::new();
+            let mut x: HashSet<usize> = HashSet::new();
             // based on bit locations
-            if bit_location[0] < arr.len() {
-                x.insert(bit_location[0], 0);
-            }
-
-            for (bit_idx, location) in bit_location.iter().enumerate().skip(1) {
+            for location in &bit_location {
                 if *location >= arr.len() {
                     continue;
                 }
-                let mut value = x.get(location).unwrap_or(&0).clone();
-                value += 1 << (bit_idx - 1);
-                x.insert(*location, value);
+                x.insert(*location);
             }
-            // println!("x: {:?}", x);
-            // println!("res: {:?}", res);
-            let mut keys: Vec<&usize> = x.keys().collect();
-            // println!("keys: {:?}", keys);
+            let mut keys: Vec<&usize> = x.iter().collect();
             keys.sort_by_key(|&&x| usize::MAX - x);
-            // println!("keys: {:?}", keys);
-            let mut or_val = x.get(keys[0]).unwrap().clone();
-            // println!("{:?}", or_val);
+            let mut or_val = arr[*keys[0]];
             for &key in keys.iter().skip(1) {
-                let val = x.get(key).unwrap();
+                let val = arr[*key];
                 or_val |= val;
                 res.insert(or_val);
             }
-
-            // println!();
         }
         return res.len() as i32;
     }
