139dc02824cb889df85432e65950700c06e7df08
Gabor Markowski <gabor94@gmail.com>
2025-08-24 02:11:31 +0100

    1493: simpler solution

--- 1493/src/lib.rs
+++ 1493/src/lib.rs
@@ -2,32 +2,14 @@ struct Solution {}
 
 impl Solution {
     pub fn longest_subarray(mut nums: Vec<i32>) -> i32 {
-        let nulval = usize::MAX - 10;
-        let mut res = 0;
-        let (mut prev_zero, mut start_pos) = (nulval, nulval);
-
-        for (pos, &x) in nums.iter().chain([&0, &0]).enumerate() {
+        let (mut res, mut curr, mut prev) = (0, 0, 0);
+        for &x in nums.iter().chain([&0]) {
             if x == 1 {
-                if start_pos == nulval {
-                    start_pos = pos;
-                }
+                curr += 1;
             }
             if x == 0 {
-                if (start_pos == nulval) || (prev_zero == nulval) {
-                    prev_zero = pos;
-                } else if (prev_zero + 1) == pos {
-                    // 111111111100
-                    // two zeros in a row - (prev_zero == pos-1)
-                    res = res.max(prev_zero - start_pos);
-                    start_pos = nulval;
-                    prev_zero = pos;
-                } else {
-                    // 111111111111110111111111111x
-                    // start_pos
-                    res = res.max(pos - start_pos - 1);
-                    start_pos = prev_zero + 1;
-                    prev_zero = pos;
-                }
+                res = res.max(curr + prev);
+                (prev, curr) = (curr, 0);
             }
         }
         return res.min(nums.len() - 1) as i32;
