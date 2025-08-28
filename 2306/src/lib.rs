9a1cfcaefbbef55aeed3d7446e1d1ec971810eda
Gabor Markowski <gabor94@gmail.com>
2025-08-28 22:47:35 +0100

    2306: maximal rectangle faster

--- 2306/src/lib.rs
+++ 2306/src/lib.rs
@@ -1,37 +1,27 @@
 impl Solution {
     pub fn distinct_names(ideas: Vec<String>) -> i64 {
-        let mut map: std::collections::HashMap<String, u32> = std::collections::HashMap::new();
+        let mut map: std::collections::HashMap<char, std::collections::HashSet<String>> =
+            std::collections::HashMap::new();
 
         for idea in ideas {
             let pref = idea.chars().next().unwrap();
-            let pref_shift = (pref as u8) - ('a' as u8);
             let mut suff = idea;
             suff.remove(0);
 
-            let mut val = *map.get(&suff).unwrap_or(&0);
-            val |= 1 << pref_shift;
-            map.insert(suff, val);
+            map.entry(pref)
+                .or_insert(std::collections::HashSet::new())
+                .insert(suff);
         }
 
         let mut res = 0;
-        let mut vals: Vec<u32> = map.values().cloned().collect();
-        vals.sort();
 
-        for i1 in 0..vals.len() {
-            let v1 = vals[i1];
-            let nv1 = !v1;
-            let (mut subval, mut prev) = (0, 0);
-
-            for i2 in i1 + 1..vals.len() {
-                let v2 = vals[i2];
-                if v2 != prev {
-                    subval =
-                        2 * ((v1 & !v2).count_ones() as i64) * ((v2 & nv1).count_ones() as i64);
-                    prev = v2;
-                }
-                res += subval;
+        for (idx, v1) in map.values().enumerate() {
+            for v2 in map.values().skip(idx + 1) {
+                let union_size = v1.intersection(v2).count();
+                res += 2 * ((v1.len() - union_size) as i64) * ((v2.len() - union_size) as i64);
             }
         }
+
         return res;
     }
 }
