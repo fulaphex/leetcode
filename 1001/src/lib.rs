d350e318496c6cf6a4f9026b10a0c13cf6ea85b0
Gabor Markowski <gabor94@gmail.com>
2025-08-29 01:06:28 +0100

    1001: grid illumination with counters

--- 1001/src/lib.rs
+++ 1001/src/lib.rs
@@ -1,7 +1,14 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::{AddAssign, SubAssign},
+};
 
 impl Solution {
     pub fn grid_illumination(n: i32, lamps: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        fn get_diags(row: i32, col: i32) -> (i32, i32) {
+            return (row - col, row + col);
+        }
+
         let mut res = vec![];
         let (mut row_lamps, mut col_lamps, mut diag_lamps, mut diag2_lamps) = (
             HashMap::new(),
@@ -9,44 +16,33 @@ impl Solution {
             HashMap::new(),
             HashMap::new(),
         );
-        for lamp in &lamps {
-            let (row, col) = (lamp[0], lamp[1]);
-            let (diag, diag2) = (row - col, row + col);
-            row_lamps
-                .entry(row)
-                .or_insert(HashSet::new())
-                .insert((row, col));
-            col_lamps
-                .entry(col)
-                .or_insert(HashSet::new())
-                .insert((row, col));
-            diag_lamps
-                .entry(diag)
-                .or_insert(HashSet::new())
-                .insert((row, col));
-            diag2_lamps
-                .entry(diag2)
-                .or_insert(HashSet::new())
-                .insert((row, col));
+        let mut lamp_set: HashSet<(i32, i32)> = lamps.into_iter().map(|x| (x[0], x[1])).collect();
+        for (row, col) in &lamp_set {
+            let (diag, diag2) = get_diags(*row, *col);
+            row_lamps.entry(*row).or_insert(0).add_assign(1);
+            col_lamps.entry(*col).or_insert(0).add_assign(1);
+            diag_lamps.entry(diag).or_insert(0).add_assign(1);
+            diag2_lamps.entry(diag2).or_insert(0).add_assign(1);
         }
 
         for query in &queries {
             let (row, col) = (query[0], query[1]);
-            let (diag, diag2) = (row - col, row + col);
-            if row_lamps.get(&row).unwrap_or(&HashSet::new()).len() > 0 {
-                res.push(1);
-            } else if col_lamps.get(&col).unwrap_or(&HashSet::new()).len() > 0 {
-                res.push(1);
-            } else if diag_lamps.get(&diag).unwrap_or(&HashSet::new()).len() > 0 {
-                res.push(1);
-            } else if diag2_lamps.get(&diag2).unwrap_or(&HashSet::new()).len() > 0 {
+            let (diag, diag2) = get_diags(row, col);
+            if row_lamps.get(&row).unwrap_or(&0) > &0
+                || col_lamps.get(&col).unwrap_or(&0) > &0
+                || diag_lamps.get(&diag).unwrap_or(&0) > &0
+                || diag2_lamps.get(&diag2).unwrap_or(&0) > &0
+            {
                 res.push(1);
             } else {
                 res.push(0);
             }
             for r in (row - 1)..=(row + 1) {
                 for c in (col - 1)..=(col + 1) {
-                    let (d, d2) = (r - c, r + c);
+                    if !lamp_set.remove(&(r, c)) {
+                        continue;
+                    }
+                    let (d, d2) = get_diags(r, c);
                     Self::rem_light(&(r, c), r, &mut row_lamps);
                     Self::rem_light(&(r, c), c, &mut col_lamps);
                     Self::rem_light(&(r, c), d, &mut diag_lamps);
@@ -58,10 +54,10 @@ impl Solution {
         return res;
     }
 
-    fn rem_light(lamp: &(i32, i32), row: i32, row_lamps: &mut HashMap<i32, HashSet<(i32, i32)>>) {
-        let lamps = row_lamps.get_mut(&row);
-        if lamps.is_some() {
-            lamps.unwrap().remove(lamp);
+    fn rem_light(lamp: &(i32, i32), row: i32, row_lamps: &mut HashMap<i32, i32>) {
+        let count = row_lamps.get_mut(&row);
+        if count.is_some() {
+            count.unwrap().sub_assign(1);
         }
     }
 }
@@ -99,6 +95,30 @@ mod tests {
             .map(|x| Vec::from(x))
             .collect();
         let res = vec![1, 1, 0];
+        assert_eq!(Solution::grid_illumination(n, lamps, queries), res);
+    }
+
+    #[test]
+    fn test4() {
+        let n = 6;
+        let lamps = [
+            [2, 5],
+            [4, 2],
+            [0, 3],
+            [0, 5],
+            [1, 4],
+            [4, 2],
+            [3, 3],
+            [1, 0],
+        ]
+        .iter()
+        .map(|x| Vec::from(x))
+        .collect();
+        let queries = [[4, 3], [3, 1], [5, 3], [0, 5], [4, 4], [3, 3]]
+            .iter()
+            .map(|x| Vec::from(x))
+            .collect();
+        let res = vec![1, 0, 1, 1, 0, 1];
         assert_eq!(Solution::grid_illumination(n, lamps, queries), res);
     }
 }
