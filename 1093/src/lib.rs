28926b21cf48adfde7438d2071b11ee0efb78e2d
Gabor Markowski <gabor94@gmail.com>
2025-09-01 14:58:59 +0100

    1093: statistics from a large sample in rust

---
+++ 1093/src/lib.rs
@@ -0,0 +1,80 @@
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn sample_stats(count: Vec<i32>) -> Vec<f64> {
+        let total_count: f64 = count.iter().map(|&x| x as f64).sum();
+        let mut median_idxs =
+            VecDeque::from([(total_count as i32 + 1) / 2, (total_count as i32 + 2) / 2]);
+        let mut median_values = vec![];
+        let (mut min, mut max, mut sum, mut mode) = (300., 0., 0., (0., 0));
+        let mut seen_so_far = 0;
+
+        for (idx, &c) in count.iter().enumerate() {
+            if c > 0 {
+                if min == 300. {
+                    min = idx as f64;
+                }
+                max = idx as f64;
+                sum += (idx as f64) * (c as f64);
+                if mode.1 < c {
+                    mode = (idx as f64, c);
+                }
+            }
+            while !median_idxs.is_empty() {
+                let med_idx = median_idxs.front().unwrap();
+                if seen_so_far < *med_idx && *med_idx <= (seen_so_far + c) {
+                    median_idxs.pop_front();
+                    median_values.push(idx as f64);
+                } else {
+                    break;
+                }
+            }
+            seen_so_far += c;
+        }
+
+        let mean = sum / total_count;
+        let median = median_values.into_iter().sum::<f64>() / 2.;
+        return vec![min, max, mean, median, mode.0];
+    }
+}
+
+struct Solution {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let count = vec![
+            0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let res = [1.00000, 3.00000, 2.37500, 2.50000, 3.00000];
+        assert_eq!(Solution::sample_stats(count), res);
+    }
+
+    #[test]
+    fn test2() {
+        let count = vec![
+            0, 4, 3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ];
+        let res = [1.00000, 3.00000, 2.37500, 2.50000, 3.00000];
+        assert_eq!(Solution::sample_stats(count), res);
+    }
+}
