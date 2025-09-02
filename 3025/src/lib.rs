a359c03471072e2581a9826eaf7aff338bde80c6
Gabor Markowski <gabor94@gmail.com>
2025-09-02 02:20:58 +0100

    3025: find the number of ways to place people i but faster and better

--- 3025/src/lib.rs
+++ 3025/src/lib.rs
@@ -3,19 +3,29 @@ impl Solution {
         let mut res = 0;
         let mut points: Vec<(i32, i32)> = points.iter().map(|p| (p[0], p[1])).collect();
         points.sort_by_key(|&(x, y)| (x, -y));
-        for (ia, a) in points.iter().enumerate() {
-            for (ib, b) in points.iter().enumerate().skip(ia + 1) {
-                let mut add = true;
-                if !(a.0 <= b.0 && a.1 >= b.1) {
+
+        #[inline]
+        fn upleft((ax, ay): (i32, i32), (bx, by): (i32, i32)) -> bool {
+            // is a up-left of b
+            return ay >= by;
+        }
+
+        for (ia, &a) in points.iter().enumerate() {
+            let mut highest = i32::MIN;
+            for (idx, w) in points.windows(2).skip(ia).enumerate() {
+                let (c, b) = (w[0], w[1]);
+                if highest == a.1 {
+                    break;
+                }
+
+                if c.1 <= a.1 && idx != 0 {
+                    highest = highest.max(c.1);
+                }
+
+                if !upleft(a, b) {
                     continue;
                 }
-                for c in points.iter().take(ib).skip(ia + 1) {
-                    if (a.0 <= c.0) && (c.0 <= b.0) && (a.1 >= c.1) && (c.1 >= b.1) {
-                        add = false;
-                        break;
-                    }
-                }
-                if add {
+                if highest < b.1 {
                     res += 1;
                 }
             }
