6206ac6432f11c5cd0fc1162a644db7eb3a9e4bf
Gabor Markowski <gabor94@gmail.com>
2025-10-02 01:29:30 +0100

    221: maximal square in rust

--- 221/src/lib.rs
+++ 221/src/lib.rs
@@ -1,8 +1,8 @@
 impl Solution {
     pub fn maximal_square(matrix: Vec<Vec<char>>) -> i32 {
-        let (row_count, col_count) = (matrix.len(), matrix[0].len());
+        let col_count = matrix[0].len();
         let mut res = 0;
-        let mut dp = vec![vec![0; col_count]; row_count];
+        let mut dp = vec![vec![0; col_count]; 2];
         for (idx, &x) in matrix[0].iter().enumerate() {
             if x == '1' {
                 dp[0][idx] = 1;
@@ -10,18 +10,18 @@ impl Solution {
             }
         }
         for (row_idx, row) in matrix.iter().enumerate().skip(1) {
-            if matrix[row_idx][0] == '1' {
-                dp[row_idx][0] = 1;
-                res = res.max(1);
-            };
+            dp[row_idx % 2][0] = if matrix[row_idx][0] == '1' { 1 } else { 0 };
+            res = res.max(dp[row_idx % 2][0]);
             for (col_idx, &val) in row.iter().enumerate().skip(1) {
-                if val == '1' {
-                    dp[row_idx][col_idx] = dp[row_idx - 1][col_idx]
-                        .min(dp[row_idx][col_idx - 1])
-                        .min(dp[row_idx - 1][col_idx - 1])
-                        + 1;
-                    res = res.max(dp[row_idx][col_idx]);
-                }
+                dp[row_idx % 2][col_idx] = if val == '1' {
+                    dp[(row_idx - 1) % 2][col_idx]
+                        .min(dp[row_idx % 2][col_idx - 1])
+                        .min(dp[(row_idx - 1) % 2][col_idx - 1])
+                        + 1
+                } else {
+                    0
+                };
+                res = res.max(dp[row_idx % 2][col_idx]);
             }
         }
         return res * res;
